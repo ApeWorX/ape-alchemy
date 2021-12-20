9eba2a2db27716d5a0d78f4b054e0bb7b59e94eb
unparalleled-js <yingthi@live.com>
2021-12-20 17:25:09 -0600

    chore: prepare for beta

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     url="https://github.com/ApeWorX/ape-alchemy",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0a29",
+        "eth-ape>=0.1.0b2",
         "importlib-metadata ; python_version<'3.8'",
     ],
     python_requires=">=3.7,<4",
