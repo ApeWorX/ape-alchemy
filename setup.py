9f30226cebf5cb9f77c94a578e1de2e03d64cac6
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-04-21 12:08:20 +0200

    chore: bump ape core version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=21.10b0,<22.0",  # auto-formatter and linter
+        "black>=22.3.0,<23.0",  # auto-formatter and linter
         "mypy>=0.910,<1.0",  # Static type analyzer
         "flake8>=3.9.2,<4.0",  # Style linter
         "isort>=5.9.3,<6.0",  # Import sorting linter
@@ -54,7 +54,7 @@ setup(
     url="https://github.com/ApeWorX/ape-alchemy",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0,<0.2.0",
+        "eth-ape>=0.2.1,<0.3.0",
         "web3>=5.25.0,<6.0.0",
         "importlib-metadata ; python_version<'3.8'",
     ],
