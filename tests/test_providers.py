4be44daf01e6f03e32c221e7d313bad3524254f3
Kofi Kufuor <kofikufuor@Kofis-MacBook-Pro.local>
2022-06-26 07:08:00 +0100

    Mock the ecosystem name

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -45,7 +45,7 @@ def mock_network(mocker):
     mock = mocker.MagicMock(spec=NetworkAPI)
     mock.name = "MOCK_NETWORK"
     mock.ecosystem = mocker.MagicMock(spec=EcosystemAPI)
-    mock.ecosystem.name = "MOCK_ECOSYSTEM"
+    mock.ecosystem.name = "ethereum"
     return mock
 
 
