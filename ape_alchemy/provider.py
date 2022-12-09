cf9183230c074cd09206004e67e19307a41837e1
unparalleled-js <yingthi@live.com>
2022-12-09 09:37:19 -0600

    fix: issue with gas in top level call

--- ape_alchemy/provider.py
+++ ape_alchemy/provider.py
@@ -95,10 +95,9 @@ class Alchemy(Web3Provider, UpstreamProvider):
             yield TraceFrame.parse_obj(frame)
 
     def get_call_tree(self, txn_hash: str) -> CallTreeNode:
-        receipt = self.get_receipt(txn_hash)
         raw_trace_list = self._make_request("trace_transaction", [txn_hash])
         trace_list = ParityTraceList.parse_obj(raw_trace_list)
-        return get_calltree_from_parity_trace(trace_list, gas_cost=receipt.gas_used)
+        return get_calltree_from_parity_trace(trace_list)
 
     def get_virtual_machine_error(self, exception: Exception) -> VirtualMachineError:
         if not hasattr(exception, "args") or not len(exception.args):
