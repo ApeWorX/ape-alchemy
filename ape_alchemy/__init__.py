96371b5b874e9cc1568bd3b653b291ae880dff3d
troy (troyb.eth) <me@troyb.xyz>
2024-01-25 11:07:04 -0700

    feat: add sepolia networks (#64)

--- ape_alchemy/__init__.py
+++ ape_alchemy/__init__.py
@@ -11,14 +11,17 @@ NETWORKS = {
     "arbitrum": [
         "mainnet",
         "goerli",
+        "sepolia",
     ],
     "base": [
         "mainnet",
         "goerli",
+        "sepolia",
     ],
     "optimism": [
         "mainnet",
         "goerli",
+        "sepolia",
     ],
     "polygon": [
         "mainnet",
